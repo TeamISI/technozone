b8290b9a244f694b17c8b1d3b966c530ff4ebdbb
arcayayo08 <arcadiohuesa12@gmail.com>
2020-03-27 14:08:54 +0100

    Probando base de datos

---
+++ database/insert_into_tables.sql
@@ -0,0 +1,20 @@
+INSERT INTO moviles VALUES (
+    0,
+    'Readmi Note 8 Pro',
+    'Android',
+    128,
+    6,
+    'https://www.mediamarkt.es/es/product/_m%C3%B3vil-xiaomi-redmi-note-8-pro-azul-128-gb-6-gb-ram-6-53-full-hd-helio-g90t-4500-mah-android-1469588.html',
+    249,
+    '/home/arcadio/Escritorio/informatica/Cuatrimestre2/ISI/Practicas/technozone/images/Xiaomi-Redmi-Note-8-Pro.png'
+);
+
+INSERT INTO moviles VALUES (
+    'Readmi Note 8 Pro',
+    'Android',
+    128,
+    6,
+    'https://www.mediamarkt.es/es/product/_m%C3%B3vil-xiaomi-redmi-note-8-pro-azul-128-gb-6-gb-ram-6-53-full-hd-helio-g90t-4500-mah-android-1469588.html',
+    249,
+    '/home/arcadio/Escritorio/informatica/Cuatrimestre2/ISI/Practicas/technozone/images/Xiaomi-Redmi-Note-8-Pro.png'
+);
